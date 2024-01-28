c8cbf80608f1b75d5fb830ebf32070574c67d235
pavimithra <pavimithra@gmail.com>
2024-01-28 23:35:19 +0400

    Pagination Component in  Forums Page

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from '@/store'
 import FontAwesome from '@/plugins/FontAwesome'
 import ClickOutsideDirective from '@/plugins/ClickOutsideDirective'
 import PageScrollDirective from '@/plugins/PageScrollDirective'
+import Vue3Pagination from '@/plugins/Vue3Pagination'
 
 const forumApp = createApp(App)
 forumApp.use(router)
@@ -12,6 +13,7 @@ forumApp.use(store)
 forumApp.use(FontAwesome)
 forumApp.use(ClickOutsideDirective)
 forumApp.use(PageScrollDirective)
+forumApp.use(Vue3Pagination)
 
 const requireComponent = require.context('./components', true, /App[A-Z]\w+\.(vue|js)$/)
 requireComponent.keys().forEach(function (fileName) {
--- src/store/modules/threads.js
+++ src/store/modules/threads.js
@@ -1,6 +1,7 @@
 import { findById, docToResource, makeAppendChildToParentMutation } from '@/helpers'
 import db from '@/config/firebase'
 import { doc, getDoc, collection, arrayUnion, writeBatch } from "firebase/firestore"
+import chunk from 'lodash/chunk'
 
 export default {
   namespaced: true,
@@ -87,10 +88,19 @@ export default {
       return docToResource(newThread)
     },
     fetchThread: ({ dispatch }, { id }) => dispatch('fetchItem', { emoji: '📄', resource: 'threads', id }, { root: true }),
-    fetchThreads: ({ dispatch }, { ids }) => dispatch('fetchItems', { resource: 'threads', ids, emoji: '📄' }, { root: true })
+    fetchThreads: ({ dispatch }, { ids }) => dispatch('fetchItems', { resource: 'threads', ids, emoji: '📄' }, { root: true }),
+    fetchThreadsByPage: ({ dispatch, commit }, { ids, page, perPage = 10 }) => {
+      commit('clearThreads')
+      const chunks = chunk(ids, perPage)
+      const limitedIds = chunks[page - 1]
+      return dispatch('fetchThreads', { ids: limitedIds })
+    }
   },
   mutations: {
     appendPostToThread: makeAppendChildToParentMutation({ parent: 'threads', child: 'posts' }),
-    appendContributorToThread: makeAppendChildToParentMutation({ parent: 'threads', child: 'contributors' })
+    appendContributorToThread: makeAppendChildToParentMutation({ parent: 'threads', child: 'contributors' }),
+    clearThreads (state) {
+      state.items = []
+    }
   }
 }
